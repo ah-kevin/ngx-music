6fafe9f944353e960144946c5405dd98f08b3e5e
Lennon-pro <kbjwdh@gmail.com>
2018-03-13 10:48:26 +0800

    feat(ngrx): ngrx依赖安装,基础框架搭建完成

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { RouterStateSerializer, StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
+import { CustomRouterStateSerializer } from './shared/router';
 
 
 @NgModule({
@@ -12,9 +19,22 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    /*ngrx*/
+    StoreModule.forRoot(reducers, {metaReducers}),
+    StoreRouterConnectingModule.forRoot({
+      stateKey: 'router',
+    }),
+    StoreDevtoolsModule.instrument({
+      name: 'ngx-music Store DevTools',
+      logOnly: environment.production
+    }),
+    EffectsModule.forRoot([])
   ],
-  providers: [],
+  providers: [
+    {provide: RouterStateSerializer, useClass: CustomRouterStateSerializer},
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/reducers/index.ts
@@ -0,0 +1,27 @@
+import * as fromRouter from '@ngrx/router-store';
+import { RouterStateUrl } from '../shared/router';
+import { ActionReducer, ActionReducerMap, MetaReducer } from '@ngrx/store';
+import { storeFreeze } from 'ngrx-store-freeze';
+import { environment } from '../../environments/environment';
+
+export interface State {
+  router: fromRouter.RouterReducerState<RouterStateUrl>;
+}
+
+export const reducers: ActionReducerMap<State> = {
+  router: fromRouter.routerReducer
+};
+
+// console.log all actions
+export function logger(reducer: ActionReducer<State>): ActionReducer<State> {
+  return function (state: State, action: any): State {
+    console.log('state', state);
+    console.log('action', action);
+
+    return reducer(state, action);
+  };
+}
+
+export const metaReducers: MetaReducer<State>[] = !environment.production
+  ? [storeFreeze]
+  : [];
